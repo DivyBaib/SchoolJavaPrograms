6d853a32f97fc32d3f07cd80c54a3f523b6d2efb
DivyBaib <97902700+DivyBaib@users.noreply.github.com>
2022-01-28 17:38:59 +0530

    Create Palindrome Generator

---
+++ palindrome_generator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class PalindromeGenerator
+{
+  public static void main(String [] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    int maxLimit;
+    System.out.println("Enter the positive number as max limit");
+    maxLimit=sc.nextInt();
+    String number;
+    String number_rev;
+    
+  }
+}
